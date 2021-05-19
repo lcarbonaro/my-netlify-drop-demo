3a3de4c5cfdf291ebf4bcaa54e9410a538ed4d01
Les Carbonaro <lcarbonaro@gmail.com>
2021-05-19 12:56:29 -0400

    try - testFn; move import outside handler and change to REQUIRE

--- netlify/functions/testFunc.js
+++ netlify/functions/testFunc.js
@@ -1,18 +1,13 @@
+const { createClient } = require('@supabase/supabase-js');
 
+const {SUPABASE_URL,SUPABASE_KEY} = process.env;
+
+const supabase = createClient(SUPABASE_URL,SUPABASE_KEY);
+
+console.log('supabase ok');
 
 exports.handler = async function(event, context) {
-
-    import { createClient } from '@supabase/supabase-js';
-
-    const {SUPABASE_URL,SUPABASE_KEY} = process.env;
-
-    const supabase = createClient(SUPABASE_URL,SUPABASE_KEY);
-
-    console.log('supabase ok');
-
-
-
-
+   
     return {
         statusCode: 200,
         body: JSON.stringify({hello: "world"})
