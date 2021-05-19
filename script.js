c864c17ae8c21b5f5fa3d944b50ba80a7ae3d7bc
Les Carbonaro <lcarbonaro@gmail.com>
2021-05-19 14:58:08 -0400

    fix formdata to json

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ btnSubmit.addEventListener('click', submitForm);
 
 async function submitForm() {
     let formData = new FormData(contactForm);
+    var fdObj = {};
+    formData.forEach(function(value, key){
+        fdObj[key] = value;
+    });
     let resp = await fetch('/.netlify/functions/testFunc', {
       method: 'POST',
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString()
+      body: JSON.stringify(fdObj)
     });
     //let resp = await fetch('/.netlify/functions/testFunc');
     let respJson = await resp.json();
