8e7ab5d7ecf3028c28d5f29960a32caa028539d4
Les Carbonaro <lcarbonaro@gmail.com>
2021-05-19 12:36:30 -0400

    add supabase

--- netlify/functions/testFunc.js
+++ netlify/functions/testFunc.js
@@ -1,4 +1,16 @@
+import { createClient } from '@supabase/supabase-js';
+
+const {SUPABASE_URL,SUPABASE_KEY} = process.env;
+
+const supabase = createClient(SUPABASE_URL,SUPABASE_KEY);
+
+console.log('supabase ok');
+
 exports.handler = async function(event, context) {
+
+
+
+
     return {
         statusCode: 200,
         body: JSON.stringify({hello: "world"})
