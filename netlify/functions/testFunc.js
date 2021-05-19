6e6b85bcf454c0298ae5855c499f6a4236394a53
Les Carbonaro <lcarbonaro@gmail.com>
2021-05-19 12:47:24 -0400

    try - testFn; move import inside handler

--- netlify/functions/testFunc.js
+++ netlify/functions/testFunc.js
@@ -1,12 +1,14 @@
-import { createClient } from '@supabase/supabase-js';
 
-const {SUPABASE_URL,SUPABASE_KEY} = process.env;
-
-const supabase = createClient(SUPABASE_URL,SUPABASE_KEY);
-
-console.log('supabase ok');
 
 exports.handler = async function(event, context) {
+
+    import { createClient } from '@supabase/supabase-js';
+
+    const {SUPABASE_URL,SUPABASE_KEY} = process.env;
+
+    const supabase = createClient(SUPABASE_URL,SUPABASE_KEY);
+
+    console.log('supabase ok');
 
 
 
